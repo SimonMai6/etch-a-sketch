7a7f393eaf069099a4eea48bb5a06e82fba92f21
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-05-12 17:09:30 -0700

    Made the webpage look more like a sketch
    Put the code for making a grid in a function and
    removed some code that was not needed. Still need
    to make the color stay when hovering over the
    div element.

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const container = document.querySelector("#container");
+function makeGrid(num){
+    const container = document.querySelector("#container");
+    
+    for(let i = 1; num >= i; i++){
+        const divRow = document.createElement("div");
+        container.appendChild(divRow);
 
-for(let i = 1; 16 >= i; i++){
-    const divRow = document.createElement("div");
-    // divRow.textContent = i;
-    container.appendChild(divRow);
+        for(let j = 1; num >= j; j++){
+            const divColumn = document.createElement("div");
+            divColumn.textContent = j;
+            divRow.appendChild(divColumn);
+        }
+        
 
-    for(let j = 1; 16 >= j; j++){
-        const divColumn = document.createElement("div");
-        divColumn.textContent = j;
-        divRow.appendChild(divColumn);
+    }
+}
+
+makeGrid(16);
+
+const button = document.querySelector("button");
+button.addEventListener("click", () =>{
+    const container = document.querySelector("#container");
+
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+
+    const userInput = prompt("Enter a number that is less than or equal to a hundred.", "");
+    if(100 >= userInput){
+        makeGrid(userInput);
+    }
+    else{
+        container.textContent = "Too big of a number."
     }
     
 
-}
+});
